20ef186da2f806358975de15e1eb374cc93a4f75
W_Jordan <jordan.wyts@yahoo.fr>
2021-04-26 21:03:45 +0200

    CV

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="fr" class="couleurMoutarde">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="monCV.css" rel="stylesheet"/>
+    <script src="https://kit.fontawesome.com/ebc16e5ffd.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" media="screen and (max-width: 640px)" href="monCV.css" type="text/css" />
+    <title>CV Jordan Wyts</title>
+  </head>
+  <body  class="couleurMoutarde">
+    <main>
+      <div class="cv">
+        <div class="ligne">
+          <div class="socialbtns centrer">
+            <div class="espaceHori"></div>
+            <a href="https://github.com/W-Jordan" class="fa fa-lg fa-github centrer" target="_blank"></a>
+            <a href="https://codepen.io/w-jordan" class="fa fa-lg fa-codepen centrer" target="_blank"></a>
+            <a href="https://fr.linkedin.com/in/jordan-wyts-17303019b" class="fa fa-lg fa-linkedin centrer" target="_blank"></a>
+            <a href="https://www.pinterest.fr/jBuldoJ/_saved/" class="fa fa-lg fa-pinterest centrer"target="_blank"></a>
+          </div>
+        </div>
+
+        <div class="ligne centrer">
+          <h1 class="droite">JORDAN</h1>
+          <div class="espaceVertiCentre"></div>
+          <img src="logoJW.png" alt="Logo JW">
+          <div class="espaceVertiCentre"></div>
+          <h1 class="gauche">WYTS</h1>
+        </div>
+        <div class="metier centrer"><h3>DEVELOPPEUR WEB / WEB MOBILE</h3></div>
+      
+        <div class="ligneIcone centrer">
+          <div class="espaceHori"></div>
+          <em class="fas fa-user-tie fa-4x"></em>
+        </div>
+        
+        <div class="ligne">
+          <div class="espaceHori"></div>
+          <div class="espaceVerti"></div>
+          <div class="partieGauche">
+            <h2>PROFIL</h2>
+                <span id="profil gauche">Après six années à développer des applications pour les Centrales Nucléaire, 
+                  j'ai décidé de me réorienter vers le développement web/web mobile</span>
+          </div>
+          <div class="espaceVertiCentre"></div>
+          <div class="bordureCentre couleurMoutarde"></div>
+          <div class="espaceVertiCentre"></div>
+          <div class="partieDroite">
+            <h2>CONTACT</h2>
+              <span id="contact droite">800 Verroere Straete 59470 Eringhem</br>
+                jordan.wyts@yahoo.fr</br>
+                06-21-97-01-27</br>
+              </span>
+          </div>
+          <div class="espaceVerti"></div>
+          </div>
+
+
+        
+          <div class="ligneIcone centrer">
+          <div class="espaceHori"></div>
+          <em class="fas fa-briefcase fa-4x"></em>
+        </div>
+
+         <div class="ligne">
+          <div class="espaceHori"></div>
+          <div class="espaceVerti"></div>
+          <div class="partieGauche">
+          <h2>EXPERIENCE</h2>
+            <span class="Titre">SNCMTI | 2013-Actuel</span>
+            <span class="Contenu">Développeur Informatique </br>(Centrale Nucléaire Production Energie)</span>
+            <span class="Titre">Aluminium Dunkerque | 2012-2013</span>
+            <span class="Contenu">Technicien Informatique</span>
+          </div>
+          <div class="espaceVertiCentre"></div>
+          <div class="bordureCentre couleurMoutarde"></div>
+          <div class="espaceVertiCentre"></div>
+          <div class="partieDroite">
+            <h2>FORMATION</h2>
+              <span class="Titre">Développeur Web/Web Mobile | 2021</span>
+              <span class="Contenu">Titre Professionnel de niveau 5<br/>(Mix Learning)</span>
+              <span class="Titre">BTS Informatique de gestion | 2012</span>
+              <span class="Contenu">Option Développeur d'Application</span>
+              <span class="Titre">BAS STG | 2010</span>
+              <span class="Contenu">Gestion des Systèmes d'Information</span>
+            </div>
+            <div class="espaceVerti"></div>
+          </div>
+       
+
+         <div class="ligneIcone centrer">
+          <div class="espaceHori"></div>
+          <em class="fas fa-lightbulb fa-4x"></em>
+        </div>
+
+        <div class="ligne">
+          <div class="espaceHori"></div>
+          <div class="espaceVerti"></div>
+          <div class="partieGauche">
+            <h2>SKILLS</h2>
+              <ul>
+                <li>HTML5/CSS3/PHP</li>          
+                <li>Javascript</li>
+                <li>Java</li>
+                <li>SQL (MySQL)</li>
+                <li>Git</li>
+                <li>UML/Merise</li> 
+                <li>Methode Agile</li> 
+                <li>VB.net / VBA</li>
+              <ul>
+          </div>
+
+          <div class="espaceVertiCentre"></div>
+          <div class="bordureCentre couleurMoutarde"></div>
+          <div class="espaceVertiCentre"></div>
+
+          <div class="partieDroite">
+            <h2>LOISIRS</h2>
+              <ul>
+                <li>Philatelie</li>
+                <li>Domotique (Jeedom)</li>
+                <li>Canicross</li>
+              </ul>
+          </div>
+            <div class="espaceVerti"></div>
+          </div>
+      </div>
+      </div>
+    </div>
+
+  </main>
+  </body>
+</html>
